aaf3f9540d7e211632594330a03f86d6576aa1fc
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-03 00:13:36 +0100

    v17

---
+++ project_demesol/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'DEMESOL Projects',
+    'version': '17.0.1.0.0',
+    'category': 'Project/Project',
+    'summary': "Project customizations.",
+    'description': "Default views to hide dependency tasks and my tasks by state by default",
+    'author': 'Antonio Cánovas',
+    'company': 'Punt Sistemes',
+    'maintainer': 'Punt Sistemes',
+    'website': 'https://www.ingenieriacloud.com',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/project_task_views.xml',
+    ],
+    'assets': {},
+
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
