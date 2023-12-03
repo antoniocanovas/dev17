2465578878cc286a931ccdc41ad49eb1e8fcda0f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-03 11:14:28 +0100

    v17

--- project_demesol/models/project_task.py
+++ project_demesol/models/project_task.py
@@ -10,13 +10,9 @@ class ProjectTask(models.Model):
     # Nº de tareas no terminadas de las que depende, para usar en filtro:
     @api.depends('state','depend_on_ids.state', 'active', 'depend_on_ids.active')
     def _get_depend_count(self):
-#        for record in self:
-#            total = 0
          self.pnt_depend_count =  self.env['project.task'].search_count([
                 ('id', 'in', record.depend_on_ids.ids),
                 ('state', 'not in', ['1_done', '1_canceled']),
                 ('active', '=', True)
             ])
- #           if depends.ids: total = len(depends.ids)
- #           record['pnt_depend_count'] = total
     pnt_depend_count = fields.Integer('Dependencies count', store=True, compute='_get_depend_count')
