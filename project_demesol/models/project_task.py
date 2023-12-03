c5f94efa767cc9789a1c274da6292f533ed736a3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-03 20:08:38 +0100

    v17

--- project_demesol/models/project_task.py
+++ project_demesol/models/project_task.py
@@ -8,7 +8,7 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
 
-    state = fields.Selection(selection_add = [('05_waiting_customer', "Waiting customer"), ('04_waiting_normal',)],
+    state = fields.Selection(selection_add = [('1_waiting_customer', "Waiting customer"), ('03_approved',)],
                              ondelete={'05_waiting_customer': 'set default'}
     )
 
