44d361a5ec6b8eaf0b2a5d8c73934212e30c392a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-03 01:25:04 +0100

    v17

---
+++ project_demesol/models/project_task.py
@@ -0,0 +1,21 @@
+from odoo import _, api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    @api.depends('state','depend_on_ids.state', 'active', 'depend_on_ids.active')
+    def _get_depend_count(self):
+        for record in self:
+            total = 0
+            depends = self.env['project.task'].search([
+                ('id', 'in', record.depend_on_ids.ids),
+                ('state', 'not in', ['1_done', '1_canceled']),
+                ('active', '=', True)
+            ])
+            if depends.ids: total = len(depends.ids)
+            record['pnt_depend_count'] = total
+    pnt_depend_count = fields.Integer('Dependencies count', store=True, compute='_get_depend_count')
