3cb1a136621ffdd1d9973f3a44116651aec0faf8
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-10 09:43:06 +0100

    v17

--- sale_pdf_builder/__manifest__.py
+++ sale_pdf_builder/__manifest__.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'DEMESOL Projects',
+    'name': 'Sale PDF Builder',
     'version': '17.0.1.0.0',
-    'category': 'Project/Project',
-    'summary': "Project customizations.",
-    'description': "Default views to hide dependency tasks and my tasks by state by default",
+    'category': 'Sales',
+    'summary': "PDF Template from Sale Order",
+    'description': "Add option to include sale_template from sale.order, by default only on sale.order.template",
     'author': 'Antonio Cánovas',
     'company': 'Punt Sistemes',
     'maintainer': 'Punt Sistemes',
     'website': 'https://www.ingenieriacloud.com',
     'depends': [
-        'project',
+        'sale_management',
+        'sale_pdf_quote_builder',
     ],
     'data': [
-        'views/project_task_views.xml',
+        'views/sale_order_views.xml',
     ],
     'assets': {},
 
