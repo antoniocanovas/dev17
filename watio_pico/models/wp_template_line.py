5859f4668196ddc3920c4d8cf026b21e29523eff
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-14 00:47:38 +0100

    test

--- watio_pico/models/wp_template_line.py
+++ watio_pico/models/wp_template_line.py
@@ -8,8 +8,12 @@ class WpTemplateLine(models.Model):
     _description = 'WP Template line'
 
     product_id = fields.Many2one('product.product', store=True, copy=True)
-    name = fields.Char('name', store=True, readonly=False, default=lambda s: s.product_id.name)
     wp_type = fields.Selection(string='Type', related='product_id.wp_type', store=False)
     quantity = fields.Float('Quantity', store=True, copy=True)
     factor = fields.Float('Factor', store=True, copy=True)
     wp_template_id = fields.Many2one('wp.template', store=True, readonly=True, copy=False)
+
+    @api.onchange('product_id')
+    def get_wp_template_name(self):
+        self.name = self.product_id.name
+    name = fields.Char('name', store=True, readonly=False, default=lambda self: self.product_id.name)
