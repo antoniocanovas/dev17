5730a9a70dc180839af73c83ac1e66e4adfdf32a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-14 17:46:25 +0100

    wp2

--- watio_pico/models/sale_order.py
+++ watio_pico/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models, api
+from odoo import fields, models, api, _
 
 class SsaleOrder(models.Model):
     _inherit = 'sale.order'
