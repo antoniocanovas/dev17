e84f58bc5c5232f396aa8686f62d442b83977e4e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-20 19:09:05 +0100

    plastic_tax

--- sale_inplast/models/account_move_line.py
+++ sale_inplast/models/account_move_line.py
@@ -9,3 +9,13 @@ class AccountMoveLine(models.Model):
 
     pnt_product_ids = fields.Many2many('product.product', store=False, string='Pricelist products',
                                    related='move_id.pricelist_id.pnt_product_ids')
+
+    # Campos relativos al impuesto del plático:
+    @api.depends('product_id', 'product_uom_qty')
+    def _get_plastic_unit_tax(self):
+        for record in self:
+            total = 0
+            if record.product_id and record.product_uom_qty:
+                total = record.product_id.pnt_plastic_unit_tax * record.product_uom_qty
+            record['pnt_plastic_tax'] = total
+    pnt_plastic_tax = fields.Float('Unit tax', store=False, compute='_get_plastic_unit_tax')
