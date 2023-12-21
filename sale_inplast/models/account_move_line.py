b6f651d4fc76b7a36c91f03fb32ee522a3219da8
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-21 11:39:43 +0100

    plastic_tax

--- sale_inplast/models/account_move_line.py
+++ sale_inplast/models/account_move_line.py
@@ -19,3 +19,12 @@ class AccountMoveLine(models.Model):
                 total = record.product_id.pnt_plastic_unit_tax * record.quantity
             record['pnt_plastic_tax'] = total
     pnt_plastic_tax = fields.Float('Plastic tax', store=False, compute='_get_plastic_unit_tax')
+
+    @api.depends('product_id', 'quantity')
+    def _get_plastic_kg(self):
+        for record in self:
+            total = 0
+            if record.product_id and record.quantity:
+                total = record.product_id.pnt_plastic_tax_weight * record.quantity / 1000
+            record['pnt_plastic_kg'] = total
+    pnt_plastic_kg = fields.Float('Plastic kg', store=False, compute='_get_plastic_kg')
--- sale_inplast/models/sale_order_line.py
+++ sale_inplast/models/sale_order_line.py
@@ -19,3 +19,12 @@ class SaleOrderLine(models.Model):
                 total = record.product_id.pnt_plastic_unit_tax * record.product_uom_qty
             record['pnt_plastic_tax'] = total
     pnt_plastic_tax = fields.Float('Plastic tax', store=False, compute='_get_plastic_unit_tax')
+
+    @api.depends('product_id', 'product_uom_qty')
+    def _get_plastic_kg(self):
+        for record in self:
+            total = 0
+            if record.product_id and record.product_uom_qty:
+                total = record.product_id.pnt_plastic_tax_weight * record.product_uom_qty / 1000
+            record['pnt_plastic_kg'] = total
+    pnt_plastic_kg = fields.Float('Plastic Kg', store=False, compute='_get_plastic_kg')
