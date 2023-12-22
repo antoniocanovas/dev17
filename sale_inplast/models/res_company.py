2927fe95c77a6a4a4ceb60576daf833d0c3c6b05
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-22 12:50:35 +0100

    plastic_tax

--- sale_inplast/models/res_company.py
+++ sale_inplast/models/res_company.py
@@ -16,6 +16,14 @@ class ResCompany(models.Model):
 
     pnt_pricelist_day_lock = fields.Integer('Pricelist days lock', store=True, default=15)
     pnt_update_month_day = fields.Integer('Month day to update', store=True, default=1)
-    pnt_product_plastic_tax_id = fields.Many2one('product.template', string='Plastic tax product', store=True)
-    pnt_plastic_sale_tax = fields.Float('Plastic sale tax (€/kg)', store=False, related='pnt_product_plastic_tax_id.list_price')
-    pnt_plastic_purchase_tax = fields.Float('Plastic sale tax (€/kg)', store=False, related='pnt_product_plastic_tax_id.standard_price')
+    pnt_product_plastic_tax_id = fields.Many2one('product.template', string='Plastic tax product', store=True,
+                                                 help='Selecciona un producto, ya te hemos preparado uno (busca por AEAT).'
+                                                      'Si cambian los importes del impuesto, cámbialo en este producto.')
+    pnt_plastic_sale_tax = fields.Float('Sale tax (€/kg)', store=False,
+                                        related='pnt_product_plastic_tax_id.list_price',
+                                        help='Tasa de impuesto por kg de plástico fabricado en España. '
+                                             'Es recuperable si es vendido fuera de España')
+    pnt_plastic_purchase_tax = fields.Float('Purchase tax (€/kg)', store=False,
+                                            related='pnt_product_plastic_tax_id.standard_price',
+                                            help='Tasa de impuesto por plástico no reciclable adquirido fuera de España.'
+                                                 'Es recuperable si finalmente es vendido fuera de España.')
