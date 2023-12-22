cb85c7764493c51fa89e615cd0434b308bfdd19f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-22 11:51:16 +0100

    plastic_tax

--- sale_inplast/models/account_move.py
+++ sale_inplast/models/account_move.py
@@ -7,4 +7,4 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    pnt_move_plastic_tax_id = fields.Many2many('account.move', store=True, string='Plastic tax entry')
+    pnt_move_plastic_tax_id = fields.Many2one('account.move', store=True, string='Plastic tax entry')
