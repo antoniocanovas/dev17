58e88f590c0875b4c52d331980342d14ef195df0
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-22 11:49:20 +0100

    plastic_tax

--- sale_inplast/models/__init__.py
+++ sale_inplast/models/__init__.py
@@ -8,3 +8,4 @@ from . import res_company
 from . import res_partner
 from . import sale_order
 from . import product_template
+from . import account_move
--- sale_inplast/models/product_category.py
+++ sale_inplast/models/product_category.py
@@ -8,9 +8,12 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     pnt_raw_material = fields.Many2one('product.template', store=True, copy=True, string='Raw material',
-                                       domain="[('detailed_type','=','product')]")
-    pnt_pricelist_weight = fields.Float('Pricelist weight', store=True, copy=True)
-    pnt_mrp_fault_percent = fields.Float('Fault (%)', store=True, copy=True)
+                                       domain="[('detailed_type','=','product')]",
+                                       help='Main component to manufacture these category products.')
+    pnt_pricelist_weight = fields.Float('Pricelist weight', store=True, copy=True,
+                                        help='Unit weight used to pricelist recalculation and plastic taxes.')
+    pnt_mrp_fault_percent = fields.Float('Fault (%)', store=True, copy=True,
+                                         help='Production percent deficiency')
 
     # Incremento tanto por mil debido a variaciones del coste de materia prima y energía:
     pnt_i0 = fields.Float('Inc. Raw (tanto/1000)', store=True, copy=False)
@@ -21,4 +24,5 @@ class ProductCategory(models.Model):
     # Tercer incremento en valor absoluto sobre los incrementos anteriores:
     pnt_i3 = fields.Float('Inc. 3 (€)', store=True, copy=False)
 
-    pnt_is_manufactured = fields.Boolean('Manufactured', store=True, copy=True, default=True)
+    pnt_is_manufactured = fields.Boolean('Manufactured', store=True, copy=True, default=True,
+                                         help='Enabled if products are manufactured, disabled when bought.')
