0e6026cec8e4a31ed946dbc7e24713bcaf501262
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-22 12:42:23 +0100

    plastic_tax

--- sale_inplast/models/res_company.py
+++ sale_inplast/models/res_company.py
@@ -16,5 +16,6 @@ class ResCompany(models.Model):
 
     pnt_pricelist_day_lock = fields.Integer('Pricelist days lock', store=True, default=15)
     pnt_update_month_day = fields.Integer('Month day to update', store=True, default=1)
-    pnt_plastic_tax = fields.Float('Plastic tax (€/kg)', default=0.45)
     pnt_product_plastic_tax_id = fields.Many2one('product.template', string='Plastic tax product', store=True)
+    pnt_plastic_sale_tax = fields.Float('Plastic sale tax (€/kg)', store=False, related='pnt_product_plastic_tax_id.list_price')
+    pnt_plastic_purchase_tax = fields.Float('Plastic sale tax (€/kg)', store=False, related='pnt_product_plastic_tax_id.standard_price')
