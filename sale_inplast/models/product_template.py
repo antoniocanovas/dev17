1a911eb08d2419a672e83af5b6951a6a5f709cce
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-22 12:56:02 +0100

    plastic_tax

--- sale_inplast/models/product_template.py
+++ sale_inplast/models/product_template.py
@@ -11,5 +11,5 @@ class ProductTemplate(models.Model):
     pnt_is_manufactured = fields.Boolean('Manufactured', store=True, related='categ_id.pnt_is_manufactured')
 
     def _get_plastic_unit_tax(self):
-        self.pnt_plastic_unit_tax = self.env.company.pnt_plastic_tax * self.pnt_plastic_tax_weight
+        self.pnt_plastic_unit_tax = self.env.company.pnt_plastic_sale_tax * self.pnt_plastic_tax_weight
     pnt_plastic_unit_tax = fields.Monetary('Plastic tax', store=False, compute='_get_plastic_unit_tax', digits=(3,4))
