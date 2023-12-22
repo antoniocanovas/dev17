0bf3aff7b882f87216c733ca7d6e3c363b35764a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-22 16:39:43 +0100

    plastic_tax

--- sale_inplast/models/res_company.py
+++ sale_inplast/models/res_company.py
@@ -16,6 +16,6 @@ class ResCompany(models.Model):
 
     pnt_pricelist_day_lock = fields.Integer('Pricelist days lock', store=True, default=15)
     pnt_update_month_day = fields.Integer('Pricelist update day', store=True, default=1)
-    pnt_plastic_tax = fields.Float('Sale tax (€/kg)', store=True, default=0.45,
+    pnt_plastic_tax = fields.Float('Plastics tax (€/kg)', store=True, default=0.45,
                                    help='Tasa de impuesto por kg de plástico no reciclabe fabricado en España o importado.'
                                         ' Es recuperable si es vendido fuera de España')
