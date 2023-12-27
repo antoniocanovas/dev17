cdc16c358e7ae226c7d1a5e06fe5ec88d9819956
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-27 12:01:03 +0100

    plastic_tax

--- power_cups_pnt/models/res_partner.py
+++ power_cups_pnt/models/res_partner.py
@@ -8,7 +8,7 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     pnt_power_cups_id = fields.Many2one('power.cups', string='CUPS', store=True, copy=False)
-    pnt_cadastrasl_ref = fields.Char('Cadastral ref', store=True, copy=False, tracking=True)
+    pnt_cadastral_ref = fields.Char('Cadastral ref', store=True, copy=False, tracking=True)
     pnt_partner_id = fields.Many2one('res.partner', string='Delivery address', store=False, related='pnt_power_cups_id.pnt_partner_id')
     pnt_dealer_id = fields.Many2one('res.partner', string='Dealer', store=False, related='pnt_power_cups_id.pnt_dealer_id')
     pnt_marketeer_id = fields.Many2one('res.partner', string='Marketeer', store=False, related='pnt_power_cups_id.pnt_marketeer_id')
