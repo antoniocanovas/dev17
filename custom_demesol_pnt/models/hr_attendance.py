768e419040efcf4b31d6381fa468045a1ce6fd88
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-28 08:58:37 +0100

    hr

--- custom_demesol_pnt/models/hr_attendance.py
+++ custom_demesol_pnt/models/hr_attendance.py
@@ -7,12 +7,15 @@ from datetime import timedelta
 class HrAttendance(models.Model):
     _inherit = "hr.attendance"
 
-    @api.onchange('check_in')
-    def auto_update_checkout(self):
-        if self.employee_id.auto_checkout == True:
-            self.check_out = self.check_in + timedelta(hours=8)
+ #   @api.onchange('check_in')
+ #   def auto_update_checkout(self):
+ #       if self.employee_id.auto_checkout == True:
+ #           self.check_out = self.check_in + timedelta(hours=8)
 
     @api.depends('check_in')
     def auto_update_checkout_from_kiosk(self):
+        check_out = False
         if self.employee_id.auto_checkout == True:
-            self.check_out = self.check_in + timedelta(hours=8)
+            check_out = self.check_in + timedelta(hours=8)
+        self.check_out = check_out
+    check_out = fields.Datetime('Check out', store=True, compute='auto_update_checkout_from_kiosk')
