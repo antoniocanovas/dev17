671a5251fae183d6299d63d87296fa82900859af
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-04 13:42:00 +0100

    crm docs

--- documents_crm_pnt/models/crm_lead.py
+++ documents_crm_pnt/models/crm_lead.py
@@ -7,16 +7,6 @@ class CrmLead(models.Model):
     _inherit = "crm.lead"
 
 
-    def _compute_document_count(self):
-        read_group_var = self.env['documents.document']._read_group(
-            [('partner_id', 'in', self.partner_id.ids)],
-            groupby=['partner_id'],
-            aggregates=['__count'])
-
-        document_count_dict = {partner.id: count for partner, count in read_group_var}
-        for record in self:
-            record.document_count = document_count_dict.get(record.id, 0)
-#    document_count = fields.Integer('Document Count', compute='_compute_document_count')
     document_count = fields.Integer('Document Count', related='partner_id.document_count')
 
     def action_see_documents(self):
