99e86e2cf0c137437d4d565c2ae9dfd033ed5bbb
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-05 21:27:48 +0100

    crm docs

--- project_demesol/models/project_project.py
+++ project_demesol/models/project_project.py
@@ -4,8 +4,14 @@ class ProjectProject(models.Model):
     _inherit = 'project.project'
 
     # Carpeta para Onedrive / Drive / Synology:
-    pnt_cloudfolder = fields.Char('Cloud folder', store=True, copy=False)
-    pnt_documents_folders = fields.Char('New folders', store=True, copy=True)
+    pnt_cloudfolder = fields.Char('Cloud folder', store=True, copy=False, tracking=True,
+                                  help='Folder link to other systems like Google Drive or Onedrive')
+    # Texto para indicar nuevas carpetas a crear en Odoo documents, separadas por coma.
+    pnt_documents_folders = fields.Char('New folders', store=True, copy=True, tracking=True,
+                                        help='Any word or phrase written will be created as folder in project documents. "\n"'
+                                             'You can write several folders separated by comma. "\n".'
+                                             'This system does not delete the existing folder, you can remove the text. "\n"'
+                                             'Permissions will be the same as the main project folder.')
 
     @api.onchange('pnt_documents_folders','documents_folder_id')
     def create_new_documents_folders(self):
