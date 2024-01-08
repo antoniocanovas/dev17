dd6d81ad79d2d627f2b329c2d93c2e4e1c0fa563
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-08 12:14:40 +0100

    crm docs

---
+++ project_demesol/models/project_project.py
@@ -0,0 +1,38 @@
+from odoo import _, api, fields, models
+
+class ProjectProject(models.Model):
+    _inherit = 'project.project'
+
+    # Carpeta para Onedrive / Drive / Synology:
+    pnt_cloudfolder = fields.Char('Cloud folder', store=True, copy=False, tracking=True,
+                                  help='Folder link to other systems like Google Drive or Onedrive')
+    # Texto para indicar nuevas carpetas a crear en Odoo documents, separadas por coma.
+    pnt_documents_folders = fields.Char('New folders', store=True, copy=True, tracking=True,
+                                        help='Any word or phrase written will be created as folder in project documents. \n'
+                                             'You can write several folders separated by comma. \n'
+                                             'This system does not delete the existing folder, you can remove the text. \n'
+                                             'Permissions will be the same as the main project folder.')
+
+    @api.onchange('pnt_documents_folders','documents_folder_id')
+    def create_new_documents_folders(self):
+        for record in self:
+            if (record.pnt_documents_folders) and (record.documents_folder_id.id):
+                folders = record.pnt_documents_folders.split(",")
+                for fo in folders:
+                    parent = record.documents_folder_id
+                    name = fo.lstrip()
+
+                    subfolders = fo.split("/")
+                    for subfo in subfolders:
+                        namesub = subfo.lstrip()
+                        exist = self.env['documents.folder'].search(
+                            [('name', '=', namesub), ('parent_folder_id', '=', parent.id)])
+                        if not exist.ids:
+                            newfold = self.env['documents.folder'].create({
+                                'name': namesub,
+                                'parent_folder_id': parent.id,
+                                'group_ids': parent.group_ids,
+                                'read_group_ids': parent.read_group_ids,
+                                'company_id': record.company_id.id,
+                            })
+                            parent = newfold
