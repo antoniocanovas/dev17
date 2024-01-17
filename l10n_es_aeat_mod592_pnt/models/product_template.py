db55fa20f93a8e55b9145476770abc2ad681cec2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-18 00:10:10 +0100

    parent_weight

--- l10n_es_aeat_mod592_pnt/models/product_template.py
+++ l10n_es_aeat_mod592_pnt/models/product_template.py
@@ -9,7 +9,7 @@ class ProductTemplate(models.Model):
     # Campos para el impuesto al plástico:
     @api.onchange('categ_id')
     def _get_plastic_default_weight(self):
-        self.pnt_plastic_weight = self.categ_id.pnt_pricelist_weight
+        self.pnt_plastic_weight = self.categ_id.pnt_plastic_weight
     pnt_plastic_weight = fields.Float('Plastic tax weight', compute='_get_plastic_default_weight')
 
     pnt_is_manufactured = fields.Boolean('Manufacture', store=True, related='categ_id.pnt_is_manufactured')
