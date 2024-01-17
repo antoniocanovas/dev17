16427bb0bc3748da71288eb49516738edde7642c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-17 19:10:15 +0100

    substasks

--- sale_inplast/models/sale_order.py
+++ sale_inplast/models/sale_order.py
@@ -20,6 +20,21 @@ class SaleOrder(models.Model):
     pnt_pricelist_state = fields.Selection([('active','Active'),('update','Update'),('locked','Locked')],
                                            string='Pricelist state', store=True, copy=False,
                                            compute='_get_sale_pricelist_state')
+    @api.depends('pnt_pricelist_state')
+    def _get_pricelist_state_is_update(self):
+        update = False
+        if self.pnt_pricelist_state == 'update': update = True
+        self.pnt_pricelist_is_update = update
+    pnt_pricelist_is_update = fields.Boolean('Pricelist update', store=False, compute='_get_pricelist_state_is_update')
+
+    @api.depends('pnt_pricelist_state')
+    def _get_pricelist_state_is_locked(self):
+        locked = False
+        if self.pnt_pricelist_state == 'locked': locked = True
+        self.pnt_pricelist_is_locked = locked
+    pnt_pricelist_is_locked = fields.Boolean('Pricelist locked', store=False, compute='_get_pricelist_state_is_locked')
+
+
 
 
     # Restricción para que no se puedan cambiar de estado los pedidos con tarifas bloqueadas:
