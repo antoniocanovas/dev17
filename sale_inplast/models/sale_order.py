c08ea35250cf2c8ace1688fc854790714927e9f7
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-20 09:11:52 +0100

    invoice pricelist status

--- sale_inplast/models/sale_order.py
+++ sale_inplast/models/sale_order.py
@@ -37,7 +37,7 @@ class SaleOrder(models.Model):
             if (record.invoice_status in ['no','to_invoice']) and (last_update) and (record.pnt_last_price_update < last_update):
                 required = True
             record['pnt_update_prices'] = required
-    pnt_update_prices = fields.Boolean('Update prices', store=False, compute='_get_update_prices_required')
+    pnt_update_prices = fields.Boolean('Update prices', store=False, compute='_get_sale_update_prices_required')
 
     # Restricción para que no se puedan cambiar de estado los pedidos con tarifas bloqueadas:
     @api.constrains('state')
