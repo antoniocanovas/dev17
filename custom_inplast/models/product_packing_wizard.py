e5eb79d473429de89a610cd5e8cf270000030efb
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-21 16:27:13 +0100

    custom product wizard

---
+++ custom_inplast/models/product_packing_wizard.py
@@ -0,0 +1,34 @@
+from odoo import _, api, fields, models
+
+
+class ProductPackingWizard(models.TransientModel):
+    _name = 'product.packing.wizard'
+    _description = 'Product Packing Wizard'
+
+    #Campos con duda
+    name = fields.Char('Name')
+    #Campos
+    pnt_box_type_id = fields.Many2one('product.template', string='Box', domain="[('pnt_product_type','=','packaging')]")
+    pnt_box_base_qty = fields.Integer('Base qty')
+    pnt_box_bag_id = fields.Many2one('product.template', string='Box Bag', domain="[('pnt_product_type','=','packaging')]",
+                                     default=lambda self: self.env.company.pnt_box_bag_id.id)
+    pnt_box_bag_qty = fields.Integer('Bags qty', default="1")
+    pnt_box_label_id = fields.Many2one('product.template', string='Box Label', domain="[('pnt_product_type','=','packaging')]")
+    pnt_box_label_qty = fields.Integer('Label qty', default="1")
+    pnt_box_seal_id = fields.Many2one('product.template', string='Box Seal', domain="[('pnt_product_type','=','packaging')]")
+    pnt_box_seal_qty = fields.Integer('Seal qty')
+
+    pnt_pallet_type_id = fields.Many2one('product.template', string='Pallet', domain="[('pnt_product_type','=','packaging')]")
+    pnt_pallet_box_qty = fields.Integer('Box qty', default="24")
+    pnt_pallet_film_id = fields.Many2one('product.template', string='Pallet Film', domain="[('pnt_product_type','=','packaging')]")
+    pnt_pallet_film_qty = fields.Integer('Film qty')
+    pnt_pallet_seal_id = fields.Many2one('product.template', string='Pallet seal', domain="[('pnt_product_type','=','packaging')]")
+    pnt_pallet_seal_qty = fields.Integer('Pallet seal qty')
+    pnt_pallet_label_id = fields.Many2one('product.template', string='Pallet Label', domain="[('pnt_product_type','=','packaging')]")
+    pnt_pallet_label_qty = fields.Integer('Pallet Label qty', default="1")
+    pnt_picking_label_id = fields.Many2one('product.template', string='Picking Label', domain="[('pnt_product_type','=','packaging')]")
+    pnt_picking_label_qty = fields.Integer('Picking Label qty', default="1")
+
+(poner en parámetros de empresa la bolsa defecto para cajas),
+1xEtiqueta, precinto, metros de precinto.
+
