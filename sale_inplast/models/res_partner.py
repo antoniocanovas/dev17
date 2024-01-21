8db59717f358d13bf798e4a2f67471de1d7c10b6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-21 09:40:51 +0100

    invoice pricelist status

--- sale_inplast/models/res_partner.py
+++ sale_inplast/models/res_partner.py
@@ -8,5 +8,5 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     property_product_pricelist = fields.Many2one('product.pricelist', store=True, index=True)
-    pnt_pricelist_state = fields.Selection(related='property_product_pricelist.pnt_state')
+    pnt_pricelist_state = fields.Selection(related='property_product_pricelist.pnt_state', store=True)
     pnt_next_update = fields.Date(related='property_product_pricelist.pnt_next_update')
