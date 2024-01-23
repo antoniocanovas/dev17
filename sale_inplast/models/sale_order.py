d96e41a56cfc2f889ae0f612d4c05733ffaa55be
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-23 11:09:03 +0100

    sale constrains

--- sale_inplast/models/sale_order.py
+++ sale_inplast/models/sale_order.py
@@ -45,5 +45,5 @@ class SaleOrder(models.Model):
         for record in self:
             if record.pnt_pricelist_state == 'locked':
                 raise UserError('Pedido bloqueado, revisa y actualiza la tarifa del cliente: ' + record.partner_id.name)
-            if (record.pnt_update_prices) and (record.state in ['draft','sent','cancel']):
+            if (record.pnt_update_prices) and (record.state in ['sent','cancel']):
                 raise UserError('Precios obsoletos, se requiere actualizar precios: ' + record.partner_id.name)
