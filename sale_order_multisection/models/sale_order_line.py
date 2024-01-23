a31be5d2b9a037565759771bdae0d3a981225a3f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-01-23 18:18:28 +0100

    sale constrains

--- sale_order_multisection/models/sale_order_line.py
+++ sale_order_multisection/models/sale_order_line.py
@@ -8,6 +8,7 @@ _logger = logging.getLogger(__name__)
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    enable_multisection = fields.Boolean(related='order_id.enable_multisection', store=False)
     partner_id = fields.Many2one('res.partner', related='order_id.partner_id', readonly=True)
     tax_country_id = fields.Many2one('res.country', related='order_id.tax_country_id', readonly=True)
     pricelist_id = fields.Many2one('product.pricelist', related='order_id.pricelist_id', readonly=True)
