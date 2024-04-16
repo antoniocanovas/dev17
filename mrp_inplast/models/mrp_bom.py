5dc24b727c4ea37b1697306e5e1bcb13dcd28645
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-16 16:43:26 +0200

    mrp inplast

--- mrp_inplast/models/mrp_bom.py
+++ mrp_inplast/models/mrp_bom.py
@@ -24,9 +24,9 @@ class MrpBom(models.Model):
                     ('category_id', '=', record.pnt_raw_type_id.id),
                     ('uom_type', '=', 'reference')])
                 for li in record.bom_line_ids:
-                    if (record.pnt_raw_percent != 0) and (li.product_uom_category_id == record.pnt_raw_type_id):
+                    if (record.pnt_raw_percent != 0) and (li.product_uom_category_id == li.pnt_raw_type_id):
                         factor = uom_ref._compute_quantity(record.pnt_raw_qty, li.product_id.uom_id)
-            li['product_qty'] = li.bom_product_qty * li.pnt_raw_percent / 100 * factor
+                    li['product_qty'] = li.bom_product_qty * li.pnt_raw_percent / 100 * factor
 
     @api.depends('product_tmpl_id', 'pnt_raw_type_id')
     def _get_default_uom(self):
