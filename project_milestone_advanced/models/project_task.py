3c075461e21e3ed4fd788a20a36d0ff8d98ddc15
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-20 17:44:59 +0200

    pmilestoneadv

--- project_milestone_advanced/models/project_task.py
+++ project_milestone_advanced/models/project_task.py
@@ -10,6 +10,6 @@ class ProjectTask(models.Model):
     def _get_is_pending(self):
         for record in self:
             solution = 1
-            if self.state in ['1_done', '1_canceled']: solution = 0
+            if record.state in ['1_done', '1_canceled']: solution = 0
             record['pending'] = solution
     pending = fields.Integer(store=True, copy=False, string="Pending", readonly=True, compute="_get_is_pending")
