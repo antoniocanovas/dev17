d820adf080ab01c49a9aa46f7e32097ed8196a81
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-20 17:39:16 +0200

    pmilestoneadv

---
+++ project_milestone_advanced/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Project Milestone advanced',
+    'version': '17.0.1.0.0',
+    'category': 'Project/Project',
+    'summary': "Project Milestone menu and task count",
+    'description': "New general menu."
+                   "Task done by milestone",
+    'author': 'Serincloud',
+    'company': 'Serincloud',
+    'maintainer': 'Serincloud',
+    'website': 'https://www.ingenieriacloud.com',
+    'depends': [
+        'project',
+    ],
+    'data': [],
+    'assets': {},
+
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ project_milestone_advanced/models/project_task.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    @api.depends('state')
+    def _get_is_pending(self):
+        solution = 1
+        if self.state in ['1_done', '1_canceled']: solution = 0
+        self.pending = solution
+    pending = fields.Integer(store=True, copy=False, string="Pending", readonly=True, compute="_get_is_pending")
