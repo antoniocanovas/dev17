202ef93c3af08b6c1a56d2788fe7f58d14a4efec
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-04-22 13:36:47 +0200

    pmilestoneadv

--- project_milestone_advanced/models/project_task.py
+++ project_milestone_advanced/models/project_task.py
@@ -12,6 +12,6 @@ class ProjectTask(models.Model):
             solution = 1
             if record.state in ['1_done', '1_canceled']: solution = 0
             record['pending'] = solution
-    pending = fields.Integer(store=True, copy=False, string="Pending", readonly=True, compute="_get_is_pending")
+    pending = fields.Integer(store=True, copy=False, string="Pending tasks", readonly=True, compute="_get_is_pending")
 
     milestone_name = fields.Char(string="Goal", related="milestone_id.name", store=True)
