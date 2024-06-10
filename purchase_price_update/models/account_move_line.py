c5301d4845d97fed8fe86d816a041541781b2b65
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-06-10 19:02:45 +0200

    pupv1

--- purchase_price_update/models/account_move_line.py
+++ purchase_price_update/models/account_move_line.py
@@ -7,12 +7,4 @@ class AccountMove(models.Model):
     _inherit = 'account.move.line'
 
 
-    price_update_mode = fields.Selection([('0','0'),('1','1'),('2','2')], string='PUM', default='0')
-    @api.depends('move_type')
-    def _get_is_purchase(self):
-        for record in self:
-            purchase = False
-            if record.move_type in ['in_invoice','in_refund']:
-                purchase = True
-            record['is_purchase'] = purchase
     is_purchase = fields.Boolean('Is purchase', compute='_get_is_purchase')
