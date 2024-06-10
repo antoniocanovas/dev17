3b4893d047050e6f11e07890d269ec95e9d35970
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-06-10 10:54:50 +0200

    pupv1

--- purchase_price_update/models/account_move_line.py
+++ purchase_price_update/models/account_move_line.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api
+from odoo import models, api, fields
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = 'account.move.line'
 
 
     price_update_mode = fields.Selection(selection=[('0','0'),('1','1'),('2','2')], string='PUM', default='0')
