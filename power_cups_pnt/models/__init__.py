026c019355afd111a46be69a55895c6b3036c6b3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-06-11 03:15:26 +0200

    pupv1

--- power_cups_pnt/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import power_cups
-from . import res_partner
-from . import crm_lead
-from . import project_project
-from . import project_task
-from . import sale_order
-from . import power_cups_shared
