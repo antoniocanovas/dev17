ad776b2ebba6af615b7bde6b7e56d3ca508ece6c
amunoz <amunoz@puntsistemes.es>
2024-08-12 11:23:36 +0000

    mrp boxes

--- mrp_inplast/wizard/Confirm_Delete_Lot_Wizard.py
+++ mrp_inplast/wizard/Confirm_Delete_Lot_Wizard.py
@@ -1,37 +1,64 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
+
 class ConfirmDeleteLotWizard(models.TransientModel):
     _name = 'confirm.delete.lot.wizard'
     _description = 'Confirm Delete Lot Wizard'
 
-    lot_names = fields.Char(string="Lotes a eliminar", readonly=True)
+    lot_names = fields.Char(string="Lots to Delete", readonly=True)
 
     def confirm_delete(self):
-        """ Este método se ejecuta cuando se confirma la eliminación de un lote. """
+        """ This method is executed when the user confirms the deletion of a lot. """
         active_id = self.env.context.get('active_id')
         wizard = self.env['pallet.boxes.wizard'].browse(active_id)
-        if not wizard or not wizard.pnt_barcode_input:
-            raise UserError(_("No se encontró el asistente o el campo de entrada está vacío."))
+        if not wizard:
+            raise UserError(_("No pallet wizard found."))
 
-        lot_names = wizard.pnt_barcode_input.split('MO')
+        lot_names = self.lot_names.split(',')
         lot_names = [lot_name.strip() for lot_name in lot_names if lot_name.strip()]
 
+        non_existing_lots = []
         for lot_name in lot_names:
-            lot_name_full = 'MO' + lot_name
-            lot_to_remove = self.env['stock.lot'].search([('name', '=', lot_name_full), ('parent_id', '=', wizard.pallet_id.id)])
+            lot_to_remove = self.env['stock.lot'].search([
+                ('name', '=', lot_name),
+                ('parent_id', '=', wizard.pallet_id.id)
+            ], limit=1)
             if lot_to_remove:
                 lot_to_remove.unlink()
             else:
-                raise UserError(_("El lote '%s' no fue encontrado o ya ha sido eliminado.") % lot_name_full)
+                non_existing_lots.append(lot_name)
 
-        # Limpiar el campo de entrada en el asistente original
+        # Clear the barcode input field in the original wizard
         wizard.pnt_barcode_input = ''
 
-        # Actualizar la lista de códigos de barras procesados
+        # Update the processed barcodes list
         if wizard.pallet_id:
             lots = self.env['stock.lot'].search([('parent_id', '=', wizard.pallet_id.id)])
             wizard.pnt_processed_barcodes = [(6, 0, lots.ids)]
+
+        # Prepare the notification message and refresh the wizard view
+        if non_existing_lots:
+            non_existing_message = ', '.join(non_existing_lots)
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'pallet.boxes.wizard',
+                'view_mode': 'form',
+                'res_id': wizard.id,
+                'target': 'new',
+                'context': self.env.context,
+                "flags": {'form': {'action_buttons': True}},
+                "notification": {
+                    "type": "ir.actions.client",
+                    "tag": "display_notification",
+                    "params": {
+                        "title": _("Lots Deletion Summary"),
+                        "message": _("Some lots could not be deleted because they do not exist for this pallet: [%s]") % non_existing_message,
+                        "sticky": False,
+                        "type": "warning",
+                    },
+                },
+            }
 
         return {
             'type': 'ir.actions.act_window',
@@ -39,4 +66,16 @@ class ConfirmDeleteLotWizard(models.TransientModel):
             'view_mode': 'form',
             'res_id': wizard.id,
             'target': 'new',
+            'context': self.env.context,
+            "flags": {'form': {'action_buttons': True}},
+            "notification": {
+                "type": "ir.actions.client",
+                "tag": "display_notification",
+                "params": {
+                    "title": _("Lots Deleted"),
+                    "message": _("Selected lots have been successfully deleted."),
+                    "sticky": False,
+                    "type": "success",
+                },
+            },
         }
--- mrp_inplast/wizard/pallet_boxes_wizard.py
+++ mrp_inplast/wizard/pallet_boxes_wizard.py
@@ -1,6 +1,7 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
+
 class PalletBoxesWizard(models.TransientModel):
     _name = 'pallet.boxes.wizard'
     _description = 'Add boxes to Lot'
@@ -16,6 +17,7 @@ class PalletBoxesWizard(models.TransientModel):
         string="Processed Count", compute='_compute_counts', store=True)
     remaining_count = fields.Integer(
         string="Remaining to 24", compute='_compute_counts', store=True)
+    show_confirmation = fields.Boolean(default=False)
 
     @api.depends('pnt_processed_barcodes')
     def _compute_counts(self):
@@ -25,7 +27,7 @@ class PalletBoxesWizard(models.TransientModel):
 
     @api.onchange('pallet_id')
     def _onchange_pallet_id(self):
-        """ Este método se ejecuta cuando cambia el campo 'pallet_id'. """
+        """ This method executes when the 'pallet_id' field changes. """
         if self.pallet_id:
             lots = self.env['stock.lot'].search([('parent_id', '=', self.pallet_id.id)])
             self.pnt_processed_barcodes = [(6, 0, lots.ids)]
@@ -35,12 +37,12 @@ class PalletBoxesWizard(models.TransientModel):
 
     def _process_barcode_input(self):
         max_boxes = 24  # Limit for the number of boxes
-        unprocessed_lots = []
 
         for record in self:
             if not record.pnt_barcode_input:
                 continue  # Skip if no input
 
+            # Process barcode input
             line = record.pnt_barcode_input
             lots_to_process = line.split('MO')
             lots_to_process = [lot.strip() for lot in lots_to_process if lot.strip()]
@@ -48,78 +50,100 @@ class PalletBoxesWizard(models.TransientModel):
             if not lots_to_process:
                 continue  # Skip if no valid lot names
 
-            # Debug: Check the lots to process
-            print("Lots to process:", lots_to_process)
-
+            # Find the subproduct
             subproduct = self.env['product.product'].search([
                 ('pnt_product_type', '=', 'box'),
                 ('id', 'in', record.production_id.product_id.pnt_parent_id.pnt_packing_ids.ids)
             ], limit=1)
 
             if not subproduct:
-                raise UserError(_("No se encontró un subproducto con el tipo 'box'."))
+                raise UserError(_("No subproduct with type 'box' was found."))
 
             existing_lots = self.env['stock.lot'].search([('parent_id', '=', record.pallet_id.id)])
             existing_lots_count = len(existing_lots)
 
-            # Check if total exceeds max_boxes
+            # Calculate the total boxes if adding the new ones
             if existing_lots_count + len(lots_to_process) > max_boxes:
+                # Show error notification
                 return {
                     "type": "ir.actions.client",
                     "tag": "display_notification",
                     "params": {
-                        "title": _("Maximum Number of Boxes Exceeded"),
-                        "message": _("You have exceeded the limit of 24 boxes. No lots were created."),
+                        "title": _("Error"),
+                        "message": _("You have exceeded 24 boxes. No new lots were created."),
                         "sticky": False,
-                        "type": "warning",
+                        "type": "danger",
                     },
                 }
-
-            created_lots_count = 0
 
             # Try block to handle barcode processing and field updates
             try:
                 for lot in lots_to_process:
                     lot_name = 'MO' + lot
                     exist = self.env['stock.lot'].search([('name', '=', lot_name)])
-                    if record.pallet_id:
-                        record.pallet_id.related_boxes_ids = [(6, 0, record.pnt_processed_barcodes.ids)]
                     if not exist:
                         new_lot = self.env['stock.lot'].create({
                             'product_id': subproduct.id,
                             'name': lot_name,
                             'parent_id': record.pallet_id.id,
                         })
-                        created_lots_count += 1
                     # Update processed barcodes even if the lot exists
                     lots = self.env['stock.lot'].search([('parent_id', '=', record.pallet_id.id)])
                     record.pnt_processed_barcodes = [(6, 0, lots.ids)]
             except Exception as e:
-                print("Error processing barcodes:", str(e))  # Log the error
+                raise UserError(_("Error processing barcodes: %s") % str(e))
 
             # Update related boxes on the pallet
             if record.pallet_id:
                 record.pallet_id.related_boxes_ids = [(6, 0, record.pnt_processed_barcodes.ids)]
 
-        # Handle unprocessed lots after processing
-        if unprocessed_lots:
-            unprocessed_message = ', '.join(unprocessed_lots)
+    def _process_lot_removal(self):
+        """ Processes lot removal and handles notifications """
+        if not self.pnt_barcode_input:
+            raise UserError(_("Please enter a lot name to remove."))
+
+        lot_names = self.pnt_barcode_input.split('MO')
+        lot_names = [lot_name.strip() for lot_name in lot_names if lot_name.strip()]
+
+        if not lot_names:
+            raise UserError(_("Please enter at least one valid lot name."))
+
+        not_found_lots = []
+        removed_lots = []
+
+        for lot_name in lot_names:
+            lot_name_full = 'MO' + lot_name
+            lot_to_remove = self.env['stock.lot'].search(
+                [('name', '=', lot_name_full), ('parent_id', '=', self.pallet_id.id)])
+            if lot_to_remove:
+                lot_to_remove.unlink()
+                removed_lots.append(lot_name_full)
+            else:
+                not_found_lots.append(lot_name_full)
+
+        # Update pnt_barcode_input with the lots that were not found
+        if not_found_lots:
+            self.pnt_barcode_input = ', '.join(not_found_lots)
             return {
                 "type": "ir.actions.client",
                 "tag": "display_notification",
                 "params": {
-                    "title": _("Some Lots Could Not Be Created"),
-                    "message": _("The following lots could not be created: %s") % unprocessed_message,
+                    "title": _("Lots Not Found"),
+                    "message": _("The following lots were not removed because they do not exist: %s") % ', '.join(
+                        not_found_lots),
                     "sticky": False,
                     "type": "warning",
                 },
             }
 
-    def add_lots(self):
-        action = self._process_barcode_input()
-        if action:
-            self._onchange_pallet_id()
-            return action
+        # Update the processed barcodes list
+        if self.pallet_id:
+            lots = self.env['stock.lot'].search([('parent_id', '=', self.pallet_id.id)])
+            self.pnt_processed_barcodes = [(6, 0, lots.ids)]
+
+    def trigger_remove_lot(self):
+        """ Trigger removal confirmation """
+        self.show_confirmation = True
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'pallet.boxes.wizard',
@@ -128,27 +152,60 @@ class PalletBoxesWizard(models.TransientModel):
             'target': 'new',
         }
 
-    def remove_lot(self):
-        """ Este método se ejecuta cuando se hace clic en el botón "Remove Selected Lot". """
-        if not self.pnt_barcode_input:
-            raise UserError(_("Por favor, ingrese un nombre de lote para eliminar."))
+    def confirm_remove_lot(self):
+        """ Executed when the "Yes" button is clicked. """
+        try:
+            action = self._process_lot_removal()
+            if action:
+                return action
+        except UserError as e:
+            # Handle error and update the wizard to its original state
+            self._onchange_pallet_id()
+            self.show_confirmation = False
+            # Return action to display the error notification
+            return {
+                "type": "ir.actions.client",
+                "tag": "display_notification",
+                "params": {
+                    "title": _("Error"),
+                    "message": str(e),
+                    "sticky": False,
+                    "type": "danger",
+                },
+            }
+        else:
+            # Ensure to hide the confirmation dialog and update fields
+            self.show_confirmation = False
+            # Return action to refresh the wizard view
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'pallet.boxes.wizard',
+                'view_mode': 'form',
+                'res_id': self.id,
+                'target': 'new',
+            }
 
-        lot_names = self.pnt_barcode_input.split('MO')
-        lot_names = [lot_name.strip() for lot_name in lot_names if lot_name.strip()]
-
-        if not lot_names:
-            raise UserError(_("Por favor, ingrese al menos un nombre de lote válido."))
-
-        lot_names_str = ', '.join(['MO' + name for name in lot_names])
-
+    def cancel_remove_lot(self):
+        """ Executed when the "No" button is clicked. """
+        self.show_confirmation = False
+        self._onchange_pallet_id()
         return {
             'type': 'ir.actions.act_window',
-            'name': 'Confirm Delete Lot',
-            'res_model': 'confirm.delete.lot.wizard',
+            'res_model': 'pallet.boxes.wizard',
             'view_mode': 'form',
+            'res_id': self.id,
             'target': 'new',
-            'context': {
-                'default_lot_names': lot_names_str,
-                'active_id': self.id,
-            },
+        }
+
+    def add_lots(self):
+        action = self._process_barcode_input()
+        if action:
+            return action
+        self._onchange_pallet_id()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'pallet.boxes.wizard',
+            'view_mode': 'form',
+            'res_id': self.id,
+            'target': 'new',
         }
