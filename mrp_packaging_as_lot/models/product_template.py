3b123712ac63a713f7a7d580cd4774d8c79ced16
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-09-02 10:41:30 +0200

    mpr_inplast del

--- mrp_packaging_as_lot/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2023 Serincloud SL - Ingenieriacloud.com
-
-from odoo import fields, models, api
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    pnt_mrp_packaging = fields.Boolean('MRP auto packaging', default=False)
