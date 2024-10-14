5bf1a7e51fcbf86bf4af0312564dd8f27c2039c3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-14 08:33:07 +0200

    project task expiration

--- project_task_sign_request/models/sign_send_request.py
+++ project_task_sign_request/models/sign_send_request.py
@@ -3,7 +3,7 @@
 from odoo import fields, models, api
 from odoo.exceptions import UserError
 
-class SignSendRequest(models.Model):
+class SignSendRequest(models.Transient):
     _inherit = "sign.send.request"
 
     task_id = fields.Many2one('project.task', string='Task')
