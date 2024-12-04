4c91be3f4781528133c7b07712b1e161c7492a3e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-04 17:20:40 +0100

    mrp inplast multibom

--- mrp_inplast/models/stock_move_line.py
+++ mrp_inplast/models/stock_move_line.py
@@ -3,4 +3,11 @@ from odoo import _, api, fields, models
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    mrp_bom_template_id = fields.Many2one("product.bom.template", related='production_id.bom_id.mrp_bom_template_id')
+    def _get_mrp_bom_template(self):
+        for record in self:
+            bomtemplate = False
+            mrpproduction = self.env['mrp.production'].search([('name','=',self.origin)])
+            if mrpproduction.id:
+                bomtemplate = mrpproduction[0].bom_id.mrp_bom_template_id.id
+            record['mrp_bom_template_id'] = bomtemplate
+    mrp_bom_template_id = fields.Many2one("product.bom.template", compute='_get_mrp_bom_template')
