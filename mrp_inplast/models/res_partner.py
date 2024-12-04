d28219a6dcdf13306182ffb4100f1f9b313b7c93
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-04 17:32:47 +0100

    mrp inplast multibom

--- mrp_inplast/models/res_partner.py
+++ mrp_inplast/models/res_partner.py
@@ -5,3 +5,4 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     sscc_qty = fields.Integer(string="Numbers of SSCC")
+    mrp_bom_template_ids = fields.Many2many('product.bom.template', string='BOM Templates')
