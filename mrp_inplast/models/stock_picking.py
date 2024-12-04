20fdf692b7787ca040c5d102419a705f757af5a3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-04 18:33:21 +0100

    mrp inplast multibom

--- mrp_inplast/models/stock_picking.py
+++ mrp_inplast/models/stock_picking.py
@@ -6,10 +6,21 @@ class StockPicking(models.Model):
 
     def add_sscc(self):
         partner = self.partner_id or self.parent_id.partner_id  # Obtener el partner
-
         for line in self.move_line_ids:
             for sscc in range(partner.sscc_qty):
-
                 line.lot_id.get_next_sscc()
-
         return True
+
+    @api.depends('move_line_ids')
+    def _get_net_allowed_packaging(self):
+        for record in self:
+            allowed = False
+            for sm in record.move_ids_without_package:
+                for sml in sm.move_line_ids:
+                    bom_template = self.env['stock.move.line'].search(
+                        [('lot_id', '=', sml.lot_id.id), ('product_id', '=', sm.product_id.id)]).mrp_bom_template_id
+                    if (bom_template.id) and (record.partner_id.mrp_bom_template_ids.ids) and (
+                            bom_template.id not in record.partner_id.mrp_bom_template_ids.ids):
+                        allowed = True
+            record['incompatible_bom_template'] = allowed
+    incompatible_bom_template = fields.Boolean('Not allowed packaging', store=True, compute='_get_not_allowed_packaging')
