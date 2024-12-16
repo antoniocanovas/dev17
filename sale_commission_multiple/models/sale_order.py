cf303b84cf0e095d5e07fe197bd5bf70932bc910
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-16 19:44:26 +0100

    scm

--- sale_commission_multiple/models/sale_order.py
+++ sale_commission_multiple/models/sale_order.py
@@ -4,4 +4,8 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    referrer_plan_ids = fields.One2many('referrer.plan.rel', 'sale_id', string='Referrers')
+    @api.depends('partner_id')
+    def _get_partner_referrers(self):
+        self.referrer_plan_ids = [(6,0,[self.partner_id.refferrer_plan_ids.ids])]
+    referrer_plan_ids = fields.One2many('referrer.plan.rel', 'sale_id', string='Referrers', store=True,
+                                        compute='_get_partner_referrers')
