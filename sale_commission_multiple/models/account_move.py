31fa9e8feb90fb9ea83579904d50f31c0f481b9a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-16 19:56:46 +0100

    scm

--- sale_commission_multiple/models/account_move.py
+++ sale_commission_multiple/models/account_move.py
@@ -4,4 +4,19 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    referrer_plan_ids = fields.One2many('referrer.plan.rel', 'invoice_id', string='Referrers')
+
+    @api.depends('partner_id')
+    def _get_partner_referrers(self):
+        for record in self:
+            lines = []
+            if record.partner_id.referrer_plan_ids.ids:
+                for li in record.partner_id.referrer_plan_ids:
+                    newline = self.env['referrer.plan.rel'].create({
+                        'referrer_id': li.referrer_id.id,
+                        'commission_plan_id': li.commission_plan_id.id,
+                        'invoice_id': record.id,
+                    })
+                    lines.append(newline.id)
+            record['referrer_plan_ids'] = [(6,0,lines)]
+    referrer_plan_ids = fields.One2many('referrer.plan.rel', 'invoice_id', string='Referrers', store=True,
+                                        compute='_get_partner_referrers')
