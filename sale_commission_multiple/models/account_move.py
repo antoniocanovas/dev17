c0ffef90bb5847c32827fe3395af3764bcff86e7
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-16 19:18:44 +0100

    scm

--- sale_commission_multiple/models/account_move.py
+++ sale_commission_multiple/models/account_move.py
@@ -4,4 +4,4 @@ res_partner.pyfrom odoo import models, fields
 class ResPartner(models.Model):
     _inherit = "account.move"
 
-    referrer_plan_ids = fields.One2many('referrer.plan.rel', string='Referrers')
+    referrer_plan_ids = fields.One2many('referrer.plan.rel', 'invoice_id', string='Referrers')
--- sale_commission_multiple/models/sale_order.py
+++ sale_commission_multiple/models/sale_order.py
@@ -4,4 +4,4 @@ res_partner.pyfrom odoo import models, fields
 class ResPartner(models.Model):
     _inherit = "sale.order"
 
-    referrer_plan_ids = fields.One2many('referrer.plan.rel', string='Referrers')
+    referrer_plan_ids = fields.One2many('referrer.plan.rel', 'sale_id', string='Referrers')
