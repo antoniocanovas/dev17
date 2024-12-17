92734cc1acf918e1cc751ad73bbdb1c55ff0ac40
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-17 16:28:33 +0100

    scm

--- sale_commission_multiple/models/sale_order.py
+++ sale_commission_multiple/models/sale_order.py
@@ -18,7 +18,8 @@ class SaleOrder(models.Model):
                     lines.append(newline.id)
             record['referrer_plan_ids'] = [(6,0,lines)]
     referrer_plan_ids = fields.One2many('referrer.plan.rel', 'sale_id', string='Referrers', store=True,
-                                        compute='_get_partner_referrers')
+                                        #compute='_get_partner_referrers'
+                                        )
 
     """
     def _create_invoices(self):
