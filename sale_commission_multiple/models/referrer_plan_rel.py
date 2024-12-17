6c09b4c2cd92af7716a5a41814b38cf1e751132c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-17 18:24:08 +0100

    scm

--- sale_commission_multiple/models/referrer_plan_rel.py
+++ sale_commission_multiple/models/referrer_plan_rel.py
@@ -26,4 +26,5 @@ class ReferrerPlanRel(models.Model):
     @api.depends('referrer_id')
     def _get_default_commission_plan_id(self):
         for record in self:
-            record['commission_plan_id'] = record.referrer_id.commission_plan_id.id
+            if not record.id:
+                record['commission_plan_id'] = record.referrer_id.commission_plan_id.id
