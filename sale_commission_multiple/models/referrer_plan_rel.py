ed119543ddc242942bf630e2502275230fe983a7
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-17 18:02:19 +0100

    scm

--- sale_commission_multiple/models/referrer_plan_rel.py
+++ sale_commission_multiple/models/referrer_plan_rel.py
@@ -7,7 +7,8 @@ class ReferrerPlanRel(models.Model):
     _description = "Referrer plan relation"
 
     name = fields.Char("Name", compute='_get_name')
-    commission_plan_id = fields.Many2one("commission.plan", string="Commission Plan")
+    commission_plan_id = fields.Many2one("commission.plan", string="Commission Plan", store=True,
+                                         compute="_get_default_commission_plan_id")
     partner_id = fields.Many2one('res.partner', string="Customer")
     referrer_id = fields.Many2one('res.partner', string="Referrer")
     sale_id = fields.Many2one('sale.order', string="Sale order")
@@ -22,3 +23,8 @@ class ReferrerPlanRel(models.Model):
             if record.referrer_id.id:
                 name += " => " + record.referrer_id.name
             record['name'] = name
+
+    @api.depends('referrer_id')
+    def _get_default_commission_plan_id(self):
+        for record in self:
+            record['commission_plan_id'] = record.referrer_id.commission_plan_id.id
