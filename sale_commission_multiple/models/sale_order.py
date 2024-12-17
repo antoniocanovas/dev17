fba84a3d8ee55cdab4c48b1a79eb25a20383bb48
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-17 17:47:35 +0100

    scm

--- sale_commission_multiple/models/sale_order.py
+++ sale_commission_multiple/models/sale_order.py
@@ -8,6 +8,10 @@ class SaleOrder(models.Model):
     def _get_partner_referrers(self):
         for record in self:
             lines = []
+            # Para evitar error de que duplica al crear el pedido de venta:
+            if record._origin.partner_id == record.partner_id:
+                lines = record.referrer_plan_ids.ids
+            # Creación de líneas de comisionistas desde el partner:
             if record.partner_id.referrer_plan_ids.ids:
                 for li in record.partner_id.referrer_plan_ids:
                     newline = self.env['referrer.plan.rel'].create({
