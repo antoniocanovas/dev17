e3740e6a48843b4b6b9239052ab25aef14a6decb
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-17 11:56:19 +0100

    stockproductexclusive

--- sale_commission_multiple/models/sale_order.py
+++ sale_commission_multiple/models/sale_order.py
@@ -19,3 +19,19 @@ class SaleOrder(models.Model):
             record['referrer_plan_ids'] = [(6,0,lines)]
     referrer_plan_ids = fields.One2many('referrer.plan.rel', 'sale_id', string='Referrers', store=True,
                                         compute='_get_partner_referrers')
+
+    """
+    def _create_invoices(self):
+        super()._create_invoices()
+        for record in self:
+            lines = []
+            if record.referrer_plan_ids.ids:
+                for li in record.referrer_plan_ids:
+                    newline = self.env['referrer.plan.rel'].create({
+                        'referrer_id': li.referrer_id.id,
+                        'commission_plan_id': li.commission_plan_id.id,
+                        'invoice_id': nuevafactura.id,
+                    })
+                    lines.append(newline.id)
+            record['referrer_plan_ids'] = [(6,0,lines)]
+    """
