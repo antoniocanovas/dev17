0029051f6e56f50f1dbafea0f0d317b5e1424e2b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-18 16:13:19 +0100

    scm

--- sale_commission_multiple/models/account_move.py
+++ sale_commission_multiple/models/account_move.py
@@ -2,7 +2,6 @@ from odoo import models, fields, api, _
 from collections import defaultdict
 from dateutil.relativedelta import relativedelta
 from odoo.tools import formatLang, format_date
-from odoo.exceptions import UserError
 
 class AccountMove(models.Model):
     _inherit = "account.move"
