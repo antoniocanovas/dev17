ee4c6432ad08c489d6bcbc1cd3b224220aa5028f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-10 13:33:35 +0100

    analytic dist v2

--- analytic_distribution_inplast/models/res_company.py
+++ analytic_distribution_inplast/models/res_company.py
@@ -7,8 +7,8 @@ from odoo import fields, models, api
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    analytic_product_plan_id = fields.Many2one('account.analytic.plan', string='Product')
-
+    analytic_product_plan_id = fields.Many2one('account.analytic.plan', string='Product plan')
+    analytic_distribution_plan_id = fields.Many2one('account.analytic.plan', string='Distribution plan')
     # Eliminado tras reunión con Ibarra (enero 2025):
     #analytic_categ_plan_id = fields.Many2one('account.analytic.plan', string='Category')
     #analytic_department_plan_id = fields.Many2one('account.analytic.plan', string='Department')
