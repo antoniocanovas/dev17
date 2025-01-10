d73e378dddf7892dc93209a2e84c1cde7904d9aa
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-10 13:41:20 +0100

    analytic dist v2

--- analytic_distribution_inplast/models/account_move_line.py
+++ analytic_distribution_inplast/models/account_move_line.py
@@ -9,6 +9,12 @@ class AccountMoveLine(models.Model):
 
     analytic_distribution_account_id = fields.Many2one('account.analytic.account', string='Distribution',
                                                        help='Analytic distribution account')
+    analytic_distribution_plan_id = fields.Many2one('account.analytic.plan', string='Distribution plan',
+                                                    compute='_get_analytic_distribution_plan')
+
+    def _get_analytic_distribution_plan(self):
+        self.analytic_distribution_plan_id = self.env.company.analytic_distribution_plan_id.id
+
 
     # Eliminado tras reunión con Ibarra (enero 2025):
     #analytic_categ_plan_id = fields.Many2one('account.analytic.plan', string='Category')
