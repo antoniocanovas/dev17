0b7f293674a8a7dfc240df03582d3acaffe2d1f3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-13 17:09:21 +0100

    analytic dist v2

--- analytic_distribution_inplast/models/analytic_distribution.py
+++ analytic_distribution_inplast/models/analytic_distribution.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 class AnalyticDistribution(models.Model):
     _inherit = "analytic.distribution"
 
-    compute_mode = fields.Selection(
+    compute_method = fields.Selection(
         [
             ("demo", "demo INPLAST"),
             ("r13", "R13.- Electricidad"),
