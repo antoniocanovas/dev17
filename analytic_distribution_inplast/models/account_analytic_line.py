cec652ab2405ec9fbcdcd7c5721862138b59389e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-13 16:26:17 +0100

    analytic dist v2

--- analytic_distribution_inplast/models/account_analytic_line.py
+++ analytic_distribution_inplast/models/account_analytic_line.py
@@ -8,3 +8,13 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
 
+    fixed_analytic_distribution = fields.Boolean(
+        string='Fixed Analytic',
+        help='Active will be fixed distribution, variable if not.')
+
+    analytic_distribution_account_id = fields.Many2one('account.analytic.account', string='Distribution',
+                                                       help='Analytic distribution account')
+    def _get_analytic_distribution_plan(self):
+        self.analytic_distribution_plan_id = self.env.company.analytic_distribution_plan_id.id
+    analytic_distribution_plan_id = fields.Many2one('account.analytic.plan', string='Distribution plan',
+                                                    compute='_get_analytic_distribution_plan')
