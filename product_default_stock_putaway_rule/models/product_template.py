83cc4f43b1eac0d95d06f5e46b5b4f281e252453
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-01-16 09:36:58 +0100

    pdspr v1

--- product_default_stock_putaway_rule/models/product_template.py
+++ product_default_stock_putaway_rule/models/product_template.py
@@ -7,7 +7,7 @@ class ProductTemplate(models.Model):
 
     def create_default_product_stock_putaway_rule(self):
         for record in self:
-            if (record.detailed_type == "product"):
+            if (record.detailed_type == "product") and (record.product_variant_ids.ids):
                 product = record.product_variant_ids[0]
                 product_rules = self.env['stock.putaway.rule'].search([('product_id','=',product.id)])
                 if not product_rules.ids:
