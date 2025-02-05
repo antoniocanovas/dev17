0f4d725b5e5e20b0bcf9941eac1707ee23710c92
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-02-05 17:33:59 +0100

    ipnr

--- analytic_distribution_inplast/models/account_move_line.py
+++ analytic_distribution_inplast/models/account_move_line.py
@@ -7,20 +7,22 @@ from odoo import fields, models, api
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
+    # Las ventas se asignan por defecto a Producción, asignación del valor parametrizado desde res.company:
+    @api.depends('move_type')
+    def _get_analytic_distribution_account(self):
+        for record in self:
+            analytic_account = False
+            default_analytic_account = self.env.company.default_sale_analytic_distribution_account_id
+            if record.move_id.move_type in ['out_invoice','out_refund']:
+                analytic_account = default_analytic_account.id
+            record['analytic_distribution_account_id'] = analytic_account
     analytic_distribution_account_id = fields.Many2one('account.analytic.account', string='Department',
-                                                       help='Analytic distribution account')
+                                                       readonly=False, store=True,
+                                                       compute='_get_analytic_distribution_account',
+                                                       help='Analytic distribution account',
+                                                       )
     @api.depends('name')
     def _get_analytic_distribution_plan(self):
         self.analytic_distribution_plan_id = self.env.company.analytic_distribution_plan_id.id
     analytic_distribution_plan_id = fields.Many2one('account.analytic.plan', string='Distribution plan',
                                                     compute='_get_analytic_distribution_plan')
-
-
-
-    # Eliminado tras reunión con Ibarra (enero 2025):
-    #analytic_categ_plan_id = fields.Many2one('account.analytic.plan', string='Category')
-    #analytic_department_plan_id = fields.Many2one('account.analytic.plan', string='Department')
-
-    #analytic_spain_account_id = fields.Many2one('account.analytic.account', string='Spain account')
-    #analytic_eu_account_id = fields.Many2one('account.analytic.account', string='Europe account')
-    #analytic_non_eu_account_id = fields.Many2one('account.analytic.account', string='Non Europe account')
--- analytic_distribution_inplast/models/res_company.py
+++ analytic_distribution_inplast/models/res_company.py
@@ -9,6 +9,13 @@ class ResCompany(models.Model):
 
     analytic_product_plan_id = fields.Many2one('account.analytic.plan', string='Product plan')
     analytic_distribution_plan_id = fields.Many2one('account.analytic.plan', string='Distribution plan')
+    default_sale_analytic_distribution_account_id = fields.Many2one(
+        'account.analytic.account',
+        string='Sale department account',
+        help='It will be used on customer invoice lines by default.'
+    )
+
+
     # Eliminado tras reunión con Ibarra (enero 2025):
     #analytic_categ_plan_id = fields.Many2one('account.analytic.plan', string='Category')
     #analytic_department_plan_id = fields.Many2one('account.analytic.plan', string='Department')
