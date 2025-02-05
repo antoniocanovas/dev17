82f797a2634ae88dada0aac9faa1a6da2cd9b2d2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-02-05 18:46:20 +0100

    ad3

--- analytic_distribution_base/models/analytic_distribution_item.py
+++ analytic_distribution_base/models/analytic_distribution_item.py
@@ -22,3 +22,24 @@ class AnalyticDistribution(models.Model):
     analytic_line_count = fields.Integer('Lines', compute='_get_analytic_line_count')
 
     currency_id    = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id)
+
+
+    def compute_distribution(self):
+        datefrom = self.date_from
+        dateto = self.date_to
+        income_credit, income_debit, expense_credit, expense_debit = 0,0,0,0
+        incomelines = self.env['account.move.line'].search(
+            [('account_id', 'in', self.income_account_ids.ids), ('date', '>=', datefrom), ('date', '<=', dateto)])
+        for li in incomelines:
+            income_debit += li.debit
+            income_credit += li.credit
+
+        expenselines = self.env['account.move.line'].search(
+            [('account_id', 'in', self.expense_account_ids.ids), ('date', '>=', datefrom), ('date', '<=', dateto)])
+        for li in expenselines:
+            expense_debit += li.debit
+            expense_credit += li.credit
+
+        self.write(
+            {'income_debit':income_debit, 'income_credit':income_credit,
+             'expense_debit':expense_debit, 'expense_credit':expense_credit})
